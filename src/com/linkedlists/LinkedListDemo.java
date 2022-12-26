1c1333bc26ef45714d5a358d1a2930d5b45d54da
kurhadep06@gmail.com <kurhadep06@gmail.com>
2022-12-26 19:53:39 +0530

    second code: linkedlist added

---
+++ src/com/linkedlists/LinkedListDemo.java
@@ -0,0 +1,14 @@
+package com.linkedlists;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> ll= new LinkedList<String>();
+		ll.add("aa");
+		ll.add("tt");
+		System.out.println(ll);
+	}
+}
