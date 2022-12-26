833e42d1f45110d411446905e717efc3d0d498ea
kurhadep06@gmail.com <kurhadep06@gmail.com>
2022-12-26 19:48:01 +0530

    first code added: arraylist demo

---
+++ src/com/arraylists/ArrayListDemo.java
@@ -0,0 +1,14 @@
+package com.arraylists;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> al= new ArrayList<Integer>();
+		al.add(33);
+		al.add(55);
+		al.add(77);
+		System.out.println(al);
+	}
+}
